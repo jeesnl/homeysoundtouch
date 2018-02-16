1c511be68dc99399afe4a934ce02b0567f37aaf2
Stefan <stefan@stefansimon.nl>
2018-02-16 16:48:53 +0100

    Added settings page.

--- drivers/soundtouch/driver.js
+++ drivers/soundtouch/driver.js
@@ -3,29 +3,15 @@
 const Homey = require('homey');
 
 class SoundtouchDriver extends Homey.Driver {
-    onPairListDevices(data, callback ) {
-
-        let devices = [];
-        devices.push({
-            name: 'Soundtouch 10',
-            data: {
-                id: 'soundtouch10',
-                ip: '192.168.1.17',
-                port: 8090
-            }
+    onPair(socket) {
+        socket.on('save', async (data, callback) => {
+            const settings = {
+                ip: data.ip
+            };
+            callback(settings);
         });
-
-        devices.push({
-            name: 'Soundtouch 30',
-            data: {
-                id: 'soundtouch30',
-                ip: '192.168.1.2',
-                port: 8090
-            }
-        });
-
-        callback( null, devices);
     }
+
 }
 
 module.exports = SoundtouchDriver;
